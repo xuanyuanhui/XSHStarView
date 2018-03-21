2d5403a1e607c6fbabf40c70771fcdcbde70e0c4
xuanshaohui <shaohui.xuan@lifemenu.cc>
2018-03-21 14:19:55 +0800

    Initial Commit

---
+++ XSHStarView/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  XSHStarView
+//
+//  Created by 轩辕辉 on 2018/3/21.
+//  Copyright © 2018年 轩辕辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ XSHStarView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XSHStarView
+//
+//  Created by 轩辕辉 on 2018/3/21.
+//  Copyright © 2018年 轩辕辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
